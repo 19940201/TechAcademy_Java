5efa8bfd48d2da81b4ed13481d61342d3c0d24b0
19940201 <ishii039@gmail.com>
2018-07-27 03:21:40 +0900

    [Add]平成を西暦に変換して出力する処理を実装

---
+++ src/Kadai2.java
@@ -0,0 +1,10 @@
+
+public class Kadai2 {
+
+	public static void main(String[] args) {
+		int jpnCalendar = 20;
+		int ad = 1988 + jpnCalendar;
+		System.out.println("平成" + jpnCalendar + "年は西暦" + ad + "年です");
+	}
+
+}
