174ea31b3073fada65386a900a9f6691bb756ec7
19940201 <ishii039@gmail.com>
2018-07-27 03:28:15 +0900

    [Add]条件分岐を使って未成年と成人の場合で異なるメッセージを表示する処理を実装

---
+++ src/Kadai5.java
@@ -0,0 +1,21 @@
+
+public class Kadai5 {
+
+//成人の判定をするプログラム
+
+	public static void main(String[] args) {
+		int age = 5;
+
+		if (age <= 19) {
+			System.out.println("未成年です");
+		}else if (age >= 20 && age <= 29) {
+			System.out.println("20代です");
+		}else if (age >= 30 && age <= 39) {
+			System.out.println("30代です");
+		}else {
+			System.out.println("40代以降です");}
+
+
+	}
+
+}
