a116aad9549a1327ccf353b0dfd67f40826d9ae0
19940201 <ishii039@gmail.com>
2018-07-27 03:23:32 +0900

    [Add]メートルとマイルを変換して出力する処理を実装

---
+++ src/Kadai3.java
@@ -0,0 +1,15 @@
+
+public class Kadai3 {
+
+	public static void main(String[] args) {
+		int mile;
+		int meter;
+		mile = 1;
+		meter = mile * 1600;
+		System.out.println(mile +  "マイルは" + meter + "メートルです。");
+		meter = 3000;
+		mile = meter / 1600;
+		System.out.println(meter +  "メートルは" + mile + "マイルです。");
+	}
+
+}
