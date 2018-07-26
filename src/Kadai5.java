2703ba1b7072925d0035ddaad46c556a5b90e9b7
19940201 <ishii039@gmail.com>
2018-07-27 03:31:40 +0900

    [Fix]不正な値を入力した際エラー表示が出るよう修正

--- src/Kadai5.java
+++ src/Kadai5.java
@@ -5,15 +5,21 @@ public class Kadai5 {
 
 	public static void main(String[] args) {
 		int age = 5;
+		int age = -5;
 
 		if (age <= 19) {
+		if (age <= 19 && age >= 0) {
 			System.out.println("未成年です");
 		}else if (age >= 20 && age <= 29) {
 			System.out.println("20代です");
 		}else if (age >= 30 && age <= 39) {
 			System.out.println("30代です");
+		}else if (age >= 40) {
+			System.out.println("40代以降です");
 		}else {
 			System.out.println("40代以降です");}
+			System.out.println("入力エラーです");
+		}
 
 
 	}
