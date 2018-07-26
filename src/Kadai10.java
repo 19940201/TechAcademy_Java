2f7279d55becf5da3729c0da76c8b8c4f7c84212
19940201 <ishii039@gmail.com>
2018-07-27 04:21:20 +0900

    [Add]九九の表を実装

---
+++ src/Kadai10.java
@@ -0,0 +1,23 @@
+﻿
+public class Kadai10 {
+
+	public static void main(String[] args) {
+
+		System.out.print("       ");
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(String.format("%3s", 1 * i + " "));
+		}
+		System.out.println("");
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(i + "の段  ");
+			for (int j = 1; j <= 9; j++) {
+				System.out.print(String.format("%3s", i * j + " "));
+			}
+			System.out.println("");
+		}
+
+
+	}
+
+}
