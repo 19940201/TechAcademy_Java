27a2fb64c56d560f563155c6e8630c0106331a2e
19940201 <ishii039@gmail.com>
2018-07-27 03:51:14 +0900

    [Update]血液型に応じてメッセージを出力する機能を追加

--- src/Kadai7.java
+++ src/Kadai7.java
@@ -7,9 +7,26 @@ public class Kadai7 {
 		String yourBlood = "A";
 
 		if(myBlood.equals(yourBlood)) {
+		if (myBlood.equals(yourBlood)) {
 			System.out.println("あなたの思い通りに物事が進みそうです");
 		}else {
+		} else {
 			System.out.println("余裕を持った行動を心がけて");
+		}
+
+		if (yourBlood.equals("A")) {
+			System.out.println("ゆったりとした気分で過ごせそう");
+		} else if (yourBlood.equals("B")) {
+			System.out.println("なりたい自分を思い描いて");
+		} else if (yourBlood.equals("O")) {
+			System.out.println("情報収集が吉");
+		} else if (yourBlood.equals("AB")) {
+			System.out.println("本当に大切なものが見えてきそう");
+		} else {
+			if(myBlood == yourBlood) {
+				System.out.println("余裕を持った行動を心がけて");
+			}else {return;
+			}
 		}
 
 	}
