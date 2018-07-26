41e4155da60d129fb602afb310234eaf7c2b739e
19940201 <ishii039@gmail.com>
2018-07-27 03:16:03 +0900

    [Add]変数を利用して自己紹介文を出力する処理を実装

---
+++ src/kadai1.java
@@ -0,0 +1,17 @@
+
+public class kadai1 {
+	public static void main(String[] args) {
+		//プロフィール内容
+		String lastname = "山田";
+		String firstname = "太郎";
+		String sex = "男";
+		String bloodtype = "A";
+		String birthplace = "山梨県";
+		String hobby = "山登り";
+		//自己紹介文を出力
+		System.out.println("私の名前は" + lastname + firstname + "です。");
+		System.out.println("性別は" + sex + "性、" + bloodtype + "型" + "です。");
+		System.out.println("出身地は" + birthplace + "で、趣味は" + hobby + "です。");
+	}
+
+}
