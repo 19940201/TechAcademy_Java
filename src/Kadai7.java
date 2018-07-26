ef8027564bbc9f6f00bfe864cf51cdb701bfb63a
19940201 <ishii039@gmail.com>
2018-07-27 03:47:54 +0900

    [Add]二名の血液型を用いて条件分岐を行い、占いをする機能を実装

---
+++ src/Kadai7.java
@@ -0,0 +1,17 @@
+﻿
+public class Kadai7 {
+
+	public static void main(String[] args) {
+
+		String myBlood = "A";
+		String yourBlood = "A";
+
+		if(myBlood.equals(yourBlood)) {
+			System.out.println("あなたの思い通りに物事が進みそうです");
+		}else {
+			System.out.println("余裕を持った行動を心がけて");
+		}
+
+	}
+
+}
